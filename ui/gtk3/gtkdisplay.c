38b98d446358783e8df3710e63e52a7c92417364
Alberto Garcia <berto@igalia.com>
2026-06-14 19:14:15 +0200

    gtk3: Don't switch scalers when the window is resized
    Traditionally the Fuse window could only be resized in discrete steps,
    and each time that happened a new scaler was calculated to match the
    new window size.
    
    With the introduction of freely resizable windows and fractional
    scaling the Spectrum display is continually adjusted to match the new
    window size, so we don't really have to look for a new scaler.
    
    This has two advantages:
    
    - The scaler that the user selected is always preserved. Until now, a
      TV 2x scaler would turn into a normal 3x scaler if the window was
      enlarged, losing the TV filter in the process. This also happens if
      the window is put in fullscreen mode.
    
    - Without this change, changing the scaler while the window is being
      manually resized causes the screen to blink, resulting in an
      unpleasant visual artifact.
    
    The only downside of preserving the scaler is that if Fuse is opened
    with for example '-g normal' and then maximized then you get the
    fullscreen version of the 1x display (maximized by cairo) instead of
    the 4x version. This results in a blurrier image.
    
    (part of feature request #135)

--- ui/gtk3/gtkdisplay.c
+++ ui/gtk3/gtkdisplay.c
@@ -125,9 +125,6 @@ static void gtkdisplay_load_gfx_mode( void );
 static gboolean gtkdisplay_draw( GtkWidget *widget, cairo_t *cr,
                                  gpointer user_data );
 
-static gint drawing_area_resize_callback( GtkWidget *widget, GdkEvent *event,
-                                          gpointer data );
-
 static int
 init_colours( colour_format_t format )
 {
@@ -190,9 +187,6 @@ uidisplay_init( int width, int height )
                     G_CALLBACK( gtkdisplay_draw ), NULL );
 
   colour_format = FORMAT_x8r8g8b8;
-
-  g_signal_connect( G_OBJECT( gtkui_window ), "configure_event",
-                    G_CALLBACK( drawing_area_resize_callback ), NULL );
 
   error = init_colours( colour_format ); if( error ) return error;
   error = scaler_select_bitformat( BITFORMAT_X8R8G8B8 );
@@ -577,18 +571,6 @@ gtkdisplay_draw( GtkWidget *widget GCC_UNUSED, cairo_t *cr,
   return FALSE;
 }
 
-/* Called by gtkui_window on "configure_event".
-   On GTK 3 the window determines the size of the drawing area */
-static gint
-drawing_area_resize_callback( GtkWidget *widget GCC_UNUSED, GdkEvent *event,
-                              gpointer data GCC_UNUSED )
-{
-  drawing_area_resize( event->configure.width,
-                       event->configure.height - extra_height, 1 );
-
-  return FALSE;
-}
-
 void
 gtkdisplay_update_geometry( void )
 {
@@ -624,13 +606,19 @@ static void
 gtkdisplay_load_gfx_mode( void )
 {
   float scale;
+  int surface_width, surface_height;
 
   scale = scaler_get_scaling_factor( current_scaler );
 
   gtkdisplay_update_geometry();
 
-  gtk_window_resize( GTK_WINDOW( gtkui_window ), scale * image_width,
-                     scale * image_height + extra_height );
+  /* Rebuild the cairo surface for the new scaler */
+  surface_width = scale * image_width;
+  surface_height = scale * image_height;
+  drawing_area_resize( surface_width, surface_height, 0 );
+
+  gtk_window_resize( GTK_WINDOW( gtkui_window ), surface_width,
+                     surface_height + extra_height );
 
   /* Redraw the entire screen... */
   display_refresh_all();
