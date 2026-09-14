af35f46fa531846dd7ed1aca9d08e11132781e41
Fredrick Meunier <fredm@spamcop.net>
2026-09-14 23:51:25 +1000

    Extract tape block presentation

---
+++ tape_block.c
@@ -0,0 +1,210 @@
+/* tape_block.c: tape block presentation routines
+   Copyright (c) 1999-2017 Philip Kendall, Darren Salt, Witold Filipczyk
+   Copyright (c) 2015-2018 UB880D
+   Copyright (c) 2016-2021 Fredrick Meunier
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+   Author contact information:
+
+   E-mail: philip-fuse@shadowmagic.org.uk
+
+*/
+
+#include "config.h"
+
+#include <stdio.h>
+
+#include "libspectrum.h"
+
+#include "tape.h"
+
+/* Length of a standard ZX Spectrum ROM tape header block (flag byte +
+   1 type + 10 name + 2 length + 2 param1 + 2 param2 + 1 parity = 19 bytes) */
+#define TAPE_ROM_HEADER_LEN 19
+
+/* Flag byte for a ZX Spectrum ROM tape header block */
+#define TAPE_ROM_HEADER_FLAG 0x00
+
+typedef void (*tape_block_formatter)( char *buffer, size_t length,
+                                     libspectrum_tape_block *block );
+
+typedef struct tape_block_format_entry {
+  libspectrum_tape_type type;
+  tape_block_formatter formatter;
+} tape_block_format_entry;
+
+static void
+format_byte_count( char *buffer, size_t length,
+                   libspectrum_tape_block *block )
+{
+  snprintf( buffer, length, "%lu bytes",
+            (unsigned long)libspectrum_tape_block_data_length( block ) );
+}
+
+static const char *
+rom_header_type( libspectrum_byte type )
+{
+  static const char * const types[] = {
+    "Program", "Number array", "Character array", "Bytes"
+  };
+
+  return type < 4 ? types[ type ] : NULL;
+}
+
+static void
+format_rom_block( char *buffer, size_t length,
+                  libspectrum_tape_block *block )
+{
+  libspectrum_byte *data;
+  const char *type;
+  char name[ 10 * 9 + 1 ];
+
+  if( libspectrum_tape_block_data_length( block ) != TAPE_ROM_HEADER_LEN ) {
+    format_byte_count( buffer, length, block );
+    return;
+  }
+
+  data = libspectrum_tape_block_data( block );
+  if( data[0] != TAPE_ROM_HEADER_FLAG ) {
+    format_byte_count( buffer, length, block );
+    return;
+  }
+
+  type = rom_header_type( data[1] );
+  if( !type ||
+      libspectrum_zx_string_to_utf8( name, sizeof( name ), &data[2], 10 ) ) {
+    format_byte_count( buffer, length, block );
+    return;
+  }
+
+  snprintf( buffer, length, "%s: \"%s\"", type, name );
+}
+
+static void
+format_tone( char *buffer, size_t length, libspectrum_tape_block *block )
+{
+  snprintf( buffer, length, "%lu tstates",
+            (unsigned long)libspectrum_tape_block_pulse_length( block ) );
+}
+
+static void
+format_pulses( char *buffer, size_t length, libspectrum_tape_block *block )
+{
+  snprintf( buffer, length, "%lu pulses",
+            (unsigned long)libspectrum_tape_block_count( block ) );
+}
+
+static void
+format_pulse_sequence( char *buffer, size_t length,
+                       libspectrum_tape_block *block )
+{
+  size_t i;
+  unsigned long total_pulses = 0;
+
+  for( i = 0; i < libspectrum_tape_block_count( block ); i++ )
+    total_pulses += libspectrum_tape_block_pulse_repeats( block, i );
+
+  snprintf( buffer, length, "%lu pulses", total_pulses );
+}
+
+static void
+format_pause( char *buffer, size_t length, libspectrum_tape_block *block )
+{
+  snprintf( buffer, length, "%lu ms",
+            (unsigned long)libspectrum_tape_block_pause( block ) );
+}
+
+static void
+format_text( char *buffer, size_t length, libspectrum_tape_block *block )
+{
+  snprintf( buffer, length, "%s", libspectrum_tape_block_text( block ) );
+}
+
+static void
+format_jump( char *buffer, size_t length, libspectrum_tape_block *block )
+{
+  int offset = libspectrum_tape_block_offset( block );
+
+  if( offset > 0 )
+    snprintf( buffer, length, "Forward %d blocks", offset );
+  else
+    snprintf( buffer, length, "Backward %d blocks", -offset );
+}
+
+static void
+format_iterations( char *buffer, size_t length,
+                   libspectrum_tape_block *block )
+{
+  snprintf( buffer, length, "%lu iterations",
+            (unsigned long)libspectrum_tape_block_count( block ) );
+}
+
+static void
+format_options( char *buffer, size_t length, libspectrum_tape_block *block )
+{
+  snprintf( buffer, length, "%lu options",
+            (unsigned long)libspectrum_tape_block_count( block ) );
+}
+
+static void
+format_data_symbols( char *buffer, size_t length,
+                     libspectrum_tape_block *block )
+{
+  unsigned long symbols =
+    libspectrum_tape_generalised_data_symbol_table_symbols_in_block(
+      libspectrum_tape_block_data_table( block ) );
+
+  snprintf( buffer, length, "%lu data symbols", symbols );
+}
+
+static const tape_block_format_entry formatters[] = {
+  { LIBSPECTRUM_TAPE_BLOCK_ROM, format_rom_block },
+  { LIBSPECTRUM_TAPE_BLOCK_DATA_BLOCK, format_rom_block },
+  { LIBSPECTRUM_TAPE_BLOCK_TURBO, format_byte_count },
+  { LIBSPECTRUM_TAPE_BLOCK_PURE_DATA, format_byte_count },
+  { LIBSPECTRUM_TAPE_BLOCK_RAW_DATA, format_byte_count },
+  { LIBSPECTRUM_TAPE_BLOCK_PURE_TONE, format_tone },
+  { LIBSPECTRUM_TAPE_BLOCK_PULSES, format_pulses },
+  { LIBSPECTRUM_TAPE_BLOCK_PULSE_SEQUENCE, format_pulse_sequence },
+  { LIBSPECTRUM_TAPE_BLOCK_PAUSE, format_pause },
+  { LIBSPECTRUM_TAPE_BLOCK_GROUP_START, format_text },
+  { LIBSPECTRUM_TAPE_BLOCK_COMMENT, format_text },
+  { LIBSPECTRUM_TAPE_BLOCK_MESSAGE, format_text },
+  { LIBSPECTRUM_TAPE_BLOCK_CUSTOM, format_text },
+  { LIBSPECTRUM_TAPE_BLOCK_JUMP, format_jump },
+  { LIBSPECTRUM_TAPE_BLOCK_LOOP_START, format_iterations },
+  { LIBSPECTRUM_TAPE_BLOCK_SELECT, format_options },
+  { LIBSPECTRUM_TAPE_BLOCK_GENERALISED_DATA, format_data_symbols },
+};
+
+int
+tape_block_details( char *buffer, size_t length,
+                    libspectrum_tape_block *block )
+{
+  size_t i;
+  libspectrum_tape_type type = libspectrum_tape_block_type( block );
+
+  buffer[0] = '\0';
+
+  for( i = 0; i < sizeof( formatters ) / sizeof( formatters[0] ); i++ ) {
+    if( formatters[i].type == type ) {
+      formatters[i].formatter( buffer, length, block );
+      break;
+    }
+  }
+
+  return 0;
+}
